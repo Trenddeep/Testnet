af5fd0bcf975eb2ec7761a40ed1062a877b938a2
Trenddeep <75636385+Trenddeep@users.noreply.github.com>
2022-03-15 16:59:45 +0300

    Create  index.js

---
+++ index.js
@@ -0,0 +1,129 @@
+{
+    «id» : 1 ,
+    «jsonrpc» : « 2.0 » ,
+    «результат» : [
+      [
+         " краудхак " ,
+         {
+            «аккаунт» : {
+                «id» : « 1.2.15157 » ,
+                «membership_expiration_date» : « 1970-01-01T00:00:00 » ,
+                «регистратор» : « 1.2.28 » ,
+                «реферер» : « 1.2.27 » ,
+                "lifetime_referrer" : " 1.2.0 " ,
+                "network_fee_percentage" : 2000 ,
+                "life_referrer_fee_percentage" : 3000,
+                "referrer_rewards_percentage" : 0 ,
+                "name" : " crowdhack " ,
+                "владелец" :{
+                   "weight_threshold" : 1 ,
+                   "account_auths" :[],
+                   "key_auths" :[
+                     [
+                        " CWD7cbxquaRiyuoh8ii3LsoS91PzduGjnQLY9WsxLepYVbUWrZUCB " ,
+                         1
+                     ]
+                  ],
+                  "адрес_авторизации" : []
+               },
+               "активный" :{
+                   "weight_threshold" : 1 ,
+                   "account_auths" :[],
+                   "key_auths" :[
+                     [
+                        " CWD7cbxquaRiyuoh8ii3LsoS91PzduGjnQLY9WsxLepYVbUWrZUCB " ,
+                         1
+                     ]
+                  ],
+                  "адрес_авторизации" : []
+               },
+               «options» : {
+                   «memo_key» : « CWD7cbxquaRiyuoh8ii3LsoS91PzduGjnQLY9WsxLepYVbUWrZUCB » ,
+                   «voting_account» : « 1.2.5 » ,
+                   «num_witness» : 0 ,
+                   «num_committee» : 0 ,
+                   «votes» : [],
+                   «extensions» : []
+               },
+               "statistics" : " 2.6.15157 " ,
+                "whitelisting_accounts" :[],
+                "blacklisting_accounts" :[],
+                "whitelisted_accounts" :[],
+                "blacklisted_accounts" :[],
+                "owner_special_authority" :[
+                   0 ,
+                  {}
+               ],
+               "активный_специальный_авторитет" :[
+                   0 ,
+                  {}
+               ],
+               «top_n_control_flags» : 0 ,
+                «referral_levels» : 8 ,
+                «referral_status_type» : 4 ,
+                «referral_status_paid_fee» : 500000000 ,
+                «referral_status_expiration_date» : « 2021-08-13T08:41:30 » ,
+                «status_denominator» 0 : 100
+            },
+            «статистика» : {
+                «id» : « 2.6.15157 » ,
+                «владелец» : « 1.2.15157 » ,
+                «имя» : « crowdhack » ,
+                «most_recent_op» : « 2.9.9251805 » ,
+                «total_ops» : 4 ,
+                « remove_ops» : 0 ,
+                «total_core_in_orders» : 0 ,
+               "core_in_balance" : " 7100000000" ,
+                "has_cashback_vb" : false ,
+                "is_voting" : false ,
+                "last_vote_time" : " 1970-01-01T00:00:00 " ,
+                "lifetime_fees_paid" : 500000000 ,
+                "pending_fees" : 0 ,
+                "pending_vested_fees" : 0 ,
+                "personal_volume_in_period " : 0 ,
+                "network_volume_in_period" : 0 ,
+               "last_pv_update_date": « 1970-01-01T00:00:00 » ,
+                «last_nv_update_date» : « 1970-01-01T00:00:00 » ,
+                «matrix» : « 1.18.0 » ,
+                «matrix_active_levels» : 1 ,
+                «matrix_cells_opened» : 0 ,
+                "matrix_rooms_opened" : 0 ,
+                "p2p_complete_deals" : 0 ,
+                "p2p_canceled_deals" : 0 ,
+               "p2p_arbitrage_loose": 0 ,
+                "p2p_deals_volume" : 0 ,
+                "p2p_banned" : " 1970-01-01T00:00:00 " ,
+                "p2p_gateway_active" : правда ,
+                "first_month_income" : 0 ,
+                "second_month_income" : 0 ,
+                "therth_month_income" : 0 ,
+                " текущий_месяц_доход" : 0 ,
+                "общий_кредит" : 0 ,
+               "разрешено_погасить": 0 ,
+                "credit_repaid" : 0 ,
+                "кредитор" : " 1.2.0 "
+            },
+            «registrar_name» : « master » ,
+             «referrer_name» : « root » ,
+             «lifetime_referrer_name» : « commitment-account » ,
+             «votes» :[],
+             «balances» :[
+               {
+                  «id» : « 2.5.9094 » ,
+                   «владелец» : « 1.2.15157 » ,
+                   «asset_type» : « 1.3.0 » ,
+                   «баланс» : « 7100000000 » ,
+                   «maintenance_flag» : false
+               }
+            ],
+            "вестинг_балансы" :[],
+             "лимитные_ордера" :[],
+             "колл_ордера" :[],
+             "установленные_ордера" :[],
+             "предложения" :[],
+             "активы" :[],
+             "снятие средств" :[]
+         }
+      ]
+   ]
+}
